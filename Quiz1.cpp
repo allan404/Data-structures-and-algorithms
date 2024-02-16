e78916ca88a3dd610168d12c9f9ae07979f9d098
allan njoroge <115988933+allan404@users.noreply.github.com>
2024-02-16 07:40:28 +0300

    Update Quiz1.cpp

--- Quiz1.cpp
+++ Quiz1.cpp
@@ -2,6 +2,11 @@
 
 //1.Removing duplicates from a sorted array
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 int removeDuplicates(vector<int>& nums) {
     if (nums.empty()) return 0;
 
@@ -15,7 +20,25 @@ int removeDuplicates(vector<int>& nums) {
     return i + 1;
 }
 
+int main() {
+    vector<int> nums = {1, 1, 2, 2, 3, 4, 4, 5};
+    int len = removeDuplicates(nums);
+    for (int i = 0; i < len; i++) {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
+
+
+
+
 //2.Rotate Array
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 void rotate(vector<int>& nums, int k) {
     k = k % nums.size(); 
@@ -24,7 +47,25 @@ void rotate(vector<int>& nums, int k) {
     reverse(nums.begin() + k, nums.end());
 }
 
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    rotate(nums, k);
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    return 0;
+}
+
+
+
 //3.Finding Duplicates
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
 
 bool containsDuplicate(vector<int>& nums) {
     unordered_set<int> seen;
@@ -37,7 +78,18 @@ bool containsDuplicate(vector<int>& nums) {
     return false;
 }
 
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 1};
+    cout << (containsDuplicate(nums) ? "True" : "False") << endl;
+    return 0;
+}
+
 //4. Single number
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 int singleNumber(vector<int>& nums) {
     int single = 0;
@@ -46,4 +98,11 @@ int singleNumber(vector<int>& nums) {
     }
     return single;
 }
+
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+    cout << singleNumber(nums) << endl;
+    return 0;
+}
+
 
